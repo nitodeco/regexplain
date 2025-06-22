074e89c1af5159a573384a046200b9289f3ea795
nitodeco <n.moehn@outlook.com>
2025-06-22 04:03:09 +0200

    chore: add root provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { getLocale, getMessages, getTranslations } from 'next-intl/server';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import { ThemeProvider } from 'next-themes';
+import { RootProvider } from '@/components/providers/RootProvider/RootProvider';
 
 import './globals.css';
 
@@ -34,12 +34,10 @@ export default async function RootLayout({
   const [messages, locale] = await Promise.all([getMessages(), getLocale()]);
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NextIntlClientProvider messages={messages}>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-            {children}
-          </ThemeProvider>
+          <RootProvider>{children}</RootProvider>
         </NextIntlClientProvider>
       </body>
     </html>
